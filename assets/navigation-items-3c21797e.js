72fc037d5b996a5e7de6b7775a5ae5f7dea167bd
Fabio Eletto <fabio.eletto@web.de>
2024-12-30 00:09:58 +0100

    Updates

---
+++ assets/navigation-items-3c21797e.js
@@ -0,0 +1,1 @@
+const o=[{id:"home",label:"Home",mobileIcon:"fa-home"},{id:"about",label:"About",mobileIcon:["fas","user"]},{id:"projects",label:"Projects",mobileIcon:"fa-code"}],a=[{id:"back",label:"Back to portfolio",mobileLabel:"Back to portfolio",icon:["fas","arrow-left"],mobileIcon:["fas","arrow-left"]}];export{a as b,o as p};
